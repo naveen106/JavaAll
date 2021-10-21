bfd1989e7559b982775a20881b4a0088e0630303
Naveen <nk0187912@gmail.com>
2021-10-21 18:34:40 +0530

    Added countsetBits.java(amazon problem) and some concepts related questions.

---
+++ src/CheckOutput.java
@@ -0,0 +1,27 @@
+class Car {
+  private int seats;
+  private int speed;
+
+   Car(int seats, int speed) {
+    this.seats = seats;
+    this.speed = speed;
+    System.out.print("This Is Car ");
+  }
+}
+
+class Audi extends Car {
+  Audi(int x, int y) {
+    super(x,y);
+    System.out.print("This Is Para Audi ");
+  }
+  void Audi() {
+    System.out.print("This Is Def Audi");
+  }
+}
+
+public class CheckOutput {
+  public static void main(String[] args) {
+      Audi a = new Audi(5,7);
+      a.Audi();
+  }
+}
