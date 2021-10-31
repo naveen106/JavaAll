59c051365dcf1729337c1fc88be8ee222986f36a
Naveen <nk0187912@gmail.com>
2021-11-01 00:25:35 +0530

    added remove consecutive duplicates problem(recursively) solution

--- src/Recursion_II/removeConsecutiveDuplicates.java
+++ src/Recursion_II/removeConsecutiveDuplicates.java
@@ -1,6 +1,22 @@
 package Recursion_II;
 public class removeConsecutiveDuplicates {
-  public static void main(String[] args) {
 
+  public static String removeConsecutiveDuplicates(String s) {
+    if(s.length() <=1)
+      return s;
+
+    if(s.charAt(0) == s.charAt(1)){
+      return removeConsecutiveDuplicates(s.substring(1));
+    }
+
+    else{
+      String smallPart = removeConsecutiveDuplicates(s.substring(1));
+      return s.charAt(0) + smallPart;
+    }
+  }
+
+  public static void main(String[] args) {
+    String str = removeConsecutiveDuplicates("CCooddiinnggNNiinnnnjjjjaaaassss");
+    System.out.println(str);
   }
 }
