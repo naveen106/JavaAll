0b5a0a94cf49d469b3dca0eaf82363688d093272
Naveen <nk0187912@gmail.com>
2021-11-26 21:10:31 +0530

    Unfinished program

--- src/LinkedList/EliminateDuplicates.java
+++ src/LinkedList/EliminateDuplicates.java
@@ -1,12 +1,13 @@
 package LinkedList;
-class LinkedListNode<T> {
-  T data;
-  LinkedListNode<T> next;
 
-  public LinkedListNode(T data) {
-    this.data = data;
-  }
-}
+//class LinkedListNode<T> {
+//  T data;
+//  LinkedListNode<T> next;
+//
+//  public LinkedListNode(T data) {
+//    this.data = data;
+//  }
+//}
 
 public class EliminateDuplicates {
 
