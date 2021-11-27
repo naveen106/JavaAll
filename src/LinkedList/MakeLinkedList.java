8a1fc445ce000f898e61d5f3925db92d45798ffc
Naveen <nk0187912@gmail.com>
2021-11-27 23:13:04 +0530

    removed the input.close() because of unwanted behaviour while creating linked list consecutively.

--- src/LinkedList/MakeLinkedList.java
+++ src/LinkedList/MakeLinkedList.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class MakeLinkedList {
   //constructor function
   public LinkedListNode<Integer> MakeLinkedList(){
+    System.out.println("Enter numbers to make list, enter '-1' to end the list: ");
     Scanner input = new Scanner(System.in);
     int num = input.nextInt();
 
@@ -18,6 +19,7 @@ public class MakeLinkedList {
         current = current.next;
         num = input.nextInt();
     }
+ //   input.close(); //if I close the Scanner..it shows 'No such element exception'. If I make two list consecutively, then it behaves like this. see 'MergeTwoSortedLL.java';
     return head;
   }
 }
