789459b0d6687f1965434e971f1f7a3a30f85fe8
Naveen <nk0187912@gmail.com>
2021-11-27 22:48:36 +0530

    added print function

--- src/LinkedList/LinkedListNode.java
+++ src/LinkedList/LinkedListNode.java
@@ -1,10 +1,16 @@
 package LinkedList;
-
 public class LinkedListNode<T> {
   T data;
   LinkedListNode<T> next;
 
   public LinkedListNode(T data) {
     this.data = data;
+  }
+
+  public static void print(LinkedListNode<Integer> head) {
+    while (head != null){
+      System.out.print(head.data + " ");
+      head = head.next;
+    }
   }
 }
