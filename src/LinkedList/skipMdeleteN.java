688d64b15ee93d3acf1357e34646b684d45891c6
Naveen <nk0187912@gmail.com>
2021-11-30 00:43:23 +0530

    skip M nodes and delete M nodes after M nodes in a given linked-list.

--- src/LinkedList/skipMdeleteN.java
+++ src/LinkedList/skipMdeleteN.java
@@ -6,4 +6,51 @@ package LinkedList;
 
 public class skipMdeleteN {
 
+  static LinkedListNode<Integer> skipMdeleteN(LinkedListNode<Integer> head, int M, int N) {
+    if(head == null || head.next == null)
+      return head;
+
+    if(N == 0)
+      return head;
+
+    if(M == 0)
+      return null;
+
+    LinkedListNode<Integer> firstTail = head;
+    LinkedListNode<Integer> secondHead = head;
+
+    while(firstTail.next != null && secondHead.next != null){
+      int count = 0;
+
+      while(count < M-1 && firstTail.next != null){
+        firstTail = firstTail.next;
+        count++;
+      }
+
+      secondHead = firstTail.next;
+      count = 0;
+
+      while(count < N - 1 && secondHead.next != null){
+        secondHead = secondHead.next;
+        count++;
+      }
+
+      if(secondHead.next != null){
+        firstTail.next = secondHead.next;
+        firstTail = firstTail.next;
+      }
+    }
+
+    firstTail.next = null;
+    return head;
+  }
+
+  public static void main(String[] args) {
+    LinkedListNode<Integer> head = new MakeLinkedList().MakeLinkedList();
+    head = skipMdeleteN(head, 2, 3); //try with different values of M and N where 0<=M<=10^5 and 0<=N<=10^5
+    while(head.next != null){
+      System.out.println(head.data + " ");
+      head = head.next;
+    }
+  }
 }
