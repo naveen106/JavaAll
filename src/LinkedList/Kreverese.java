ed8fd8b59b7b70eb442fa28c4336735d56cc8811
Naveen <nk0187912@gmail.com>
2021-11-30 23:45:20 +0530

    Kreverse. reverse every k nodes of SLL and left out nodes should be reversed as well.

--- src/LinkedList/Kreverese.java
+++ src/LinkedList/Kreverese.java
@@ -1,36 +1,43 @@
+/*Given a singly linked list of integers, reverse the nodes of the linked list 'k' at a time and
+  return its modified list. 'k' is a positive integer and is less than or equal to the length of the linked list.
+  If the number of nodes is not a multiple of 'k,' then left-out nodes, in the end, should be reversed as well.
+
+  example:
+           Given this linked list: 1 -> 2 -> 3 -> 4 -> 5
+           For k = 2, you should return: 2 -> 1 -> 4 -> 3 -> 5
+           For k = 3, you should return: 3 -> 2 -> 1 -> 5 -> 4
+ */
 package LinkedList;
 
 public class Kreverese {
-  static LinkedListNode<Integer> kReverse(LinkedListNode<Integer> head, int k) {
-    if(head == null || head.next == null)
+  public static LinkedListNode<Integer> kReverse(LinkedListNode<Integer> head, int k) {
+    if(k == 0 || k == 1)
       return head;
 
+    if(head == null)
+      return head;
+
+    if(head.next == null)
+      return head;
+
+    LinkedListNode<Integer> current = head, prev = null, next = null;
+
     int count = 0;
-    int i = 1;
-    LinkedListNode<Integer> prev = null, current = head, next = current.next;
-    LinkedListNode<Integer> tempHead = null;
-
-    while(next != null){
-
-      if(count == k*i){
-        tempHead = current;
-        prev = current;
-        current = current.next;
-        next = current.next;
-      }
-
+    while(count < k && current != null){
+      next = current.next;
       current.next = prev;
       prev = current;
       current = next;
-      next = current.next;
-      i++;
       count++;
     }
 
-    return tempHead;
+    if(next != null)
+      head.next = kReverse(next, k);
+    return prev;
   }
   public static void main(String[] args) {
     LinkedListNode<Integer> head = new MakeLinkedList().MakeLinkedList();
+    head = kReverse(head, 6);
     while(head != null){
       System.out.print(head.data + " ");
       head = head.next;
