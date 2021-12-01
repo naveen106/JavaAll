cb50d17413d30cab2b15b4e40b31fa0ee5e900de
Naveen <nk0187912@gmail.com>
2021-12-01 20:46:41 +0530

    sort the list like  bubbleSort algorithm.

--- src/LinkedList/BubbleSortLL.java
+++ src/LinkedList/BubbleSortLL.java
@@ -1,5 +1,6 @@
 package LinkedList;
 public class BubbleSortLL {
+  /*
   //method overloading
 //  static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head, LinkedListNode<Integer> tail, LinkedListNode<Integer>smallest){
 //
@@ -21,9 +22,12 @@ public class BubbleSortLL {
 //    temp = bubbleSort(head, tail, smallest);
 //    return temp;
 //  }
+///////////////////// THIS IS MY APPROACH, WHICH I WAS TRYING(RECURSIVE), BUT WASN'T ABLE TO...AS YOU CAN SEE IT IS A LITTLE MESSY.
+///////////////////// I'LL TRY TO DO THIS QUESTION LIKE THIS...LATER.
+
   static LinkedListNode<Integer> tail = null;  //come back here later if error, to check value of tail.
   static LinkedListNode<Integer> smallest = null;
-  static int length = -1;
+  static int length = 0;
   static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
 
     //getting the smallest node and the tail node(the greatest node).
@@ -62,13 +66,61 @@ public class BubbleSortLL {
 
 
       temp.next = bubbleSort(head);
-      length--;
       LinkedListNode<Integer> newHead = temp;
-
+      length--;
       if(length >= 0)
       temp.next = bubbleSort(newHead);
 
     return temp;
+  }
+*/
+
+  /////////////////////////NOW RECURSIVE WAY ///////////////////////////////
+  private static int length(LinkedListNode<Integer> head){
+    int count = 1;
+    while(head.next != null){
+      head = head.next;
+      count++;
+    }
+    return count;
+  }
+
+  public static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
+
+    if(head==null || head.next==null)
+      return head;
+
+    int length = length(head) - 1;
+
+    for(int i=0; i<length; i++){
+      LinkedListNode<Integer> prev = null, curr = head, next = curr.next;
+
+      while(curr.next != null){
+
+        if(curr.data > curr.next.data){
+          if(prev == null){
+            curr.next = next.next;
+            next.next = curr;
+            prev = next;
+            head = prev;
+          }
+
+          else{
+            next = curr.next;
+            curr.next = next.next;
+            prev.next = next;
+            next.next = curr;
+            prev = next;
+          }
+        }
+
+        else{
+          prev = curr;
+          curr = curr.next;
+        }
+      }
+    }
+    return head;
   }
 
   public static void main(String[] args) {
@@ -79,6 +131,5 @@ public class BubbleSortLL {
       System.out.print(head.data + " ");
       head = head.next;
     }
-  //  System.out.println(head.data);
   }
 }
