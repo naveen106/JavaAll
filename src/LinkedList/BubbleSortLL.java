96a895232055d4289ecb5e106b7e31c680f8ea7c
Naveen <nk0187912@gmail.com>
2021-12-01 10:33:49 +0530

    curretnly finding smallest and greatest in the list

--- src/LinkedList/BubbleSortLL.java
+++ src/LinkedList/BubbleSortLL.java
@@ -1,4 +1,19 @@
 package LinkedList;
 
 public class BubbleSortLL {
+  public static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
+    LinkedListNode<Integer> swapFirst = head, pointer = head;
+    LinkedListNode<Integer> smallest = null, greatest = null;
+
+    while (pointer.next != null) {
+      if (pointer.data < pointer.next.data)
+        smallest = pointer;
+      else
+        greatest = pointer;
+    }
+    return smallest;
+  }
+  public static void main(String[] args) {
+
+  }
 }
