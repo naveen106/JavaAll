df098ef3f9160a31a1a190c393add5f0f6b5c630
Naveen <nk0187912@gmail.com>
2021-12-01 20:27:39 +0530

    recursively swapping the nodes, incomplete right now.

--- src/LinkedList/BubbleSortLL.java
+++ src/LinkedList/BubbleSortLL.java
@@ -23,30 +23,32 @@ public class BubbleSortLL {
 //  }
   static LinkedListNode<Integer> tail = null;  //come back here later if error, to check value of tail.
   static LinkedListNode<Integer> smallest = null;
+  static int length = -1;
   static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
 
     //getting the smallest node and the tail node(the greatest node).
-    if(smallest == null){
+    if (smallest == null) {
       LinkedListNode<Integer> pointer = head.next;
       smallest = tail = head;
 
-      while(pointer != null){
-        if(pointer.data < smallest.data) //finding the smallest node to return at the end.
-        smallest = pointer;
+      while (pointer != null) {
+        if (pointer.data < smallest.data) //finding the smallest node to return at the end.
+          smallest = pointer;
 
-        else if(pointer.data > tail.data)
-        tail = pointer;
+        else if (pointer.data > tail.data)
+          tail = pointer;
 
         pointer = pointer.next;
+        length++;
       }
     }
 
-    if(head == null || tail.next == null && head == tail) {
+    if (head == null || tail.next == null && head == tail) {
       tail = head;
       return head;
     }
 
-    if(head.next == null || head.next == tail) {
+    if (head.next == null || head.next == tail) {
       tail = head;
       return head;
     }
@@ -58,30 +60,15 @@ public class BubbleSortLL {
       head.next = temp2;
     }
 
-      temp.next = bubbleSort(head);
-      LinkedListNode<Integer> newHead = temp;
-/*
-    if(temp == smallest && tail.next != null)
-      return temp;
 
-    else {
-      head = temp;
-      temp = temp.next;
       temp.next = bubbleSort(head);
-    }
-*/
+      length--;
+      LinkedListNode<Integer> newHead = temp;
+
+      if(length >= 0)
+      temp.next = bubbleSort(newHead);
+
     return temp;
-//    LinkedListNode<Integer>smallest  = head, pointer = head.next;
-//
-//    while(pointer != null){
-//      if(pointer.data < smallest.data) //finding the smallest node to return at the end.
-//      smallest = pointer;
-//      pointer = pointer.next;
-//    }
-//
-//    LinkedListNode<Integer> newHead = temp;
-//    newHead = bubbleSort(temp, tail, smallest);
-//    return newHead;
   }
 
   public static void main(String[] args) {
