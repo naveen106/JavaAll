7cce63380bfc1e0e1656ae03e67ca7f5201fb478
Naveen <nk0187912@gmail.com>
2021-12-01 19:41:08 +0530

    recursively swapping the nodes, incomplete right now.

--- src/LinkedList/BubbleSortLL.java
+++ src/LinkedList/BubbleSortLL.java
@@ -21,28 +21,55 @@ public class BubbleSortLL {
 //    temp = bubbleSort(head, tail, smallest);
 //    return temp;
 //  }
+  static LinkedListNode<Integer> tail = null;  //come back here later if error, to check value of tail.
+  static LinkedListNode<Integer> smallest = null;
+  static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head) {
 
-  static LinkedListNode<Integer> bubbleSort(LinkedListNode<Integer> head){
-    LinkedListNode<Integer>tail = null;  //come back here later if error, to check value of tail.
+    //getting the smallest node and the tail node(the greatest node).
+    if(smallest == null){
+      LinkedListNode<Integer> pointer = head.next;
+      smallest = tail = head;
 
-    if(head == null){
+      while(pointer != null){
+        if(pointer.data < smallest.data) //finding the smallest node to return at the end.
+        smallest = pointer;
+
+        else if(pointer.data > tail.data)
+        tail = pointer;
+
+        pointer = pointer.next;
+      }
+    }
+
+    if(head == null || tail.next == null && head == tail) {
       tail = head;
       return head;
     }
 
-    if(head.next == null) {
+    if(head.next == null || head.next == tail) {
       tail = head;
       return head;
     }
 
     LinkedListNode<Integer> temp = head.next, temp2;
 
-    if(head.next.data < head.data){
+    if (head.next.data < head.data) {
       temp2 = head.next.next;
       head.next = temp2;
     }
 
-    temp.next = bubbleSort(head);
+      temp.next = bubbleSort(head);
+      LinkedListNode<Integer> newHead = temp;
+/*
+    if(temp == smallest && tail.next != null)
+      return temp;
+
+    else {
+      head = temp;
+      temp = temp.next;
+      temp.next = bubbleSort(head);
+    }
+*/
     return temp;
 //    LinkedListNode<Integer>smallest  = head, pointer = head.next;
 //
