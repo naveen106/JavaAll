454ac8df5e08e01095f9b67587715d1d159f26f3
Naveen <nk0187912@gmail.com>
2021-12-02 03:04:51 +0530

    unfinished josephus problem

---
+++ src/Recursion_II/JosephusProblem.java
@@ -0,0 +1,24 @@
+package Recursion_II;
+//this problem is literally a headache...shit!
+public class JosephusProblem {
+
+  static int findSafe(int start, int n, int k, int count) {
+    if(k<1)
+
+    if(start%k == 0){
+      start = k;
+      n = n-1;
+    }
+
+    findSafe(start+1, n, k, count);
+
+  }
+  static int findSafe(int n, int k){
+    return findSafe(0,n,k,k);
+  }
+
+
+  public static void main(String[] args){
+    System.out.print(findSafe(10, 3));
+  }
+}
